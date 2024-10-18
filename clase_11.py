1d311082a6a3261d3a3a75147b7a1e50350a9ae6
Fedecastillom <fmascm20@gmail.com>
2024-10-18 16:39:14 -0300

    clase-11

---
+++ clase_11.py
@@ -0,0 +1,34 @@
+def count(nums):
+    d = {}
+    for x in nums:
+        if x in d:
+            d[x] += 1
+        else:
+            d[x] = 1
+    for j, k in d.items():
+        print(j, k)
+
+def count2(nums):
+    counted = []
+    for i in range(len(nums)):
+        if nums[i] not in counted:
+            count = 0
+            for j in range(len(nums)):
+                if nums[i] == nums[j]:
+                    count += 1
+            print(f"{nums[i]}: {count}")
+            counted.append(nums[i])
+
+def common(list1, list2):
+    ans = []
+    for x in list1:
+        if x in list2:
+            ans.append(x)
+    return ans
+
+def prod(nums):
+    res = 0
+    for x in range(len(nums) - 1):
+        res += nums[x] * nums[x + 1]
+    return res
+print(prod([1,2,3]))
